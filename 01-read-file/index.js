1831661731d971115f99e366e00f0852684ef967
Mazayw <mazayw@gmail.com>
2022-05-14 16:19:48 +0300

    feat: add solution for task 2

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const dirPath = path.join(__dirname, 'text.txt');
-const textFile = fs.createReadStream(dirPath, 'utf8') || '';
+const textFile = fs.createReadStream(dirPath, 'utf8');
 
 textFile.on('data', function (chunk) {
   console.log(chunk);
